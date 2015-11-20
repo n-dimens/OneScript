9531572fcc798d1c08ec42812f392c3e9cfaebf9
EvilBeaver <ovsiankin.aa@gmail.com>
2015-11-20 18:53:44 +0300

    Классы-заготовки для Квалификаторов.

--- src/ScriptEngine/Machine/Contexts/TypeDescriptors.cs
+++ src/ScriptEngine/Machine/Contexts/TypeDescriptors.cs
@@ -5,82 +5,11 @@ using System.Text;
 
 namespace ScriptEngine.Machine.Contexts
 {
-    class SimpleEquatable<T>
-    {
-        protected SimpleEquatable()
-        {
-            _Equality = o => base.Equals(0);
-            _HashFunc = () => base.GetHashCode();
-        }
-
-        protected SimpleEquatable(Func<T,bool> Equality, Func<int> HashFunc)
-        {
-            _Equality = Equality;
-            _HashFunc = HashFunc;
-        }
-
-        protected Func<T, bool> _Equality;
-        protected Func<int> _HashFunc;
-
-        public override bool Equals(object obj)
-        {
-            return Equals((T)obj);
-        }
-
-        public bool Equals(T obj)
-        {
-            if (obj == null)
-                return false;
-
-            return _Equality(obj);
-        }
-
-        public override int GetHashCode()
-        {
-            return _HashFunc();
-        }
-
-        public static bool operator ==(SimpleEquatable<T> type1, T type2)
-        {
-            if (Object.ReferenceEquals(type1,null))
-            {
-                return Object.ReferenceEquals(type2, null);
-            }
-            else if (Object.ReferenceEquals(type2, null))
-            {
-                return false;
-            }
-            else
-            {
-                return type1.Equals(type2);
-            }
-        }
-
-        public static bool operator !=(SimpleEquatable<T> type1, T type2)
-        {
-            if (Object.ReferenceEquals(type1, null))
-            {
-                return !Object.ReferenceEquals(type2, null);
-            }
-            else if (Object.ReferenceEquals(type2, null))
-            {
-                return true;
-            }
-            else
-            {
-                return !type1.Equals(type2);
-            }
-
-        }
-
-
-    }
-
     public abstract class TypeQualifierContextBase : PropertyNameIndexAccessor
     {
         protected bool EqualsImpl<T>(IValue other, Func<T, bool> comparer) where T:class
         {
-            var qualifier = other as T;
+            var qualifier = other.GetRawValue() as T;
             if (qualifier == null)
                 return false;
 
@@ -111,68 +40,41 @@ namespace ScriptEngine.Machine.Contexts
             Fixed
         }
 
-        public override string ToString()
-        {
-            if (AvailableLength == AvailableLengthType.Fixed)
-            {
-                return String.Format("str({0},fixed)", Lenght.ToString());
-            }
-            else
-            {
-                return String.Format("str({0})", Lenght.ToString());
-            }
-        }
     }
 
-    sealed class V8NumberQualifier : SimpleEquatable<V8NumberQualifier>
+    public sealed class NumberQualifier : TypeQualifierContextBase
     {
 
-        public V8NumberQualifier(int IntegerPart, int Fraction = 0, bool NonNeg = false) : base()
+        public NumberQualifier(int IntegerPart, int Fraction = 0, bool NonNeg = false)
+            : base()
         {
             IntegerDigits = IntegerPart;
             FractionDigits = Fraction;
             NonNegative = NonNeg;
-
-            _Equality = (o) => IntegerDigits == o.IntegerDigits && FractionDigits == o.FractionDigits && NonNegative == o.NonNegative;
-            _HashFunc = () => ToString().GetHashCode();
-
         }
-        
+
+        public override bool Equals(IValue other)
+        {
+            return EqualsImpl<NumberQualifier>(other, (o) => IntegerDigits == o.IntegerDigits && FractionDigits == o.FractionDigits && NonNegative == o.NonNegative);
+        }
+
         public int IntegerDigits { get; private set; }
         public int FractionDigits { get; private set; }
         public bool NonNegative { get; private set; }
 
-        public override string ToString()
-        {
-            if(NonNegative)
-                return String.Format("num({0},{1},non-negative)", IntegerDigits, FractionDigits);
-            else
-                return String.Format("num({0},{1})", IntegerDigits, FractionDigits);
-        }
-
     }
 
-    sealed class V8DateQualifier : SimpleEquatable<V8DateQualifier>
+    public sealed class DateQualifier : TypeQualifierContextBase
     {
 
-        public V8DateQualifier(DateFractionsType dateFractions) : base()
+        public DateQualifier(DateFractionsType dateFractions) : base()
         {
             DateFractions = dateFractions;
+        }
 
-            _Equality = (o) => DateFractions == o.DateFractions;
-            _HashFunc = () =>
-                {
-                    switch (DateFractions)
-                    {
-                        case DateFractionsType.Date:
-                            return 2;
-                        case DateFractionsType.Time:
-                            return 1;
-                        default:
-                            return 0;
-                    }
-                };
-
+        public override bool Equals(IValue other)
+        {
+            return EqualsImpl<DateQualifier>(other, (o) => DateFractions == o.DateFractions);
         }
 
         public DateFractionsType DateFractions { get; private set; }
@@ -182,19 +84,6 @@ namespace ScriptEngine.Machine.Contexts
             DateAndTime,
             Date,
             Time
-        }
-
-        public override string ToString()
-        {
-            switch (DateFractions)
-            {
-                case DateFractionsType.Date:
-                    return "date()";
-                case DateFractionsType.Time:
-                    return "time()";
-                default:
-                    return ""; // datetime не требует доп. пояснений
-            }
         }
 
     }
