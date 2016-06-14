281ff9c629b71e93d8ca8af78d29b2c3e63b0227
Sergey Gorbunov <svgorbunov83@yandex.ru>
2016-06-14 19:32:13 +0300

    Добавлен скрипт для запуска через Psake.

---
+++ default.ps1
@@ -0,0 +1,13 @@
+Task default -Depends Build
+
+Task Build {
+    & "C:\\Program Files (x86)\\MSBuild\\12.0\\Bin\\MSBuild.exe" /nologo /v:q /filelogger BuildAll.csproj    
+}
+
+Task Tests -Depends Build {
+    & install\build\bin\oscript.exe tests\testrunner.os -runall tests
+}
+
+Task Mono {
+    & xbuild /nologo /v:q src\1Script_Mono.sln
+}
