a51c9612b5c933da0182a3405071d0e1d77dd6f5
Sergey Gorbunov <svgorbunov83@yandex.ru>
2016-06-15 11:21:56 +0300

    Добавлены пакеты NUnit, NSubstitute. Добавлен проект для тестов ScriptEngine.HostedScript

--- default.ps1
+++ default.ps1
@@ -4,7 +4,7 @@ Task Build {
     & "C:\\Program Files (x86)\\MSBuild\\12.0\\Bin\\MSBuild.exe" /nologo /v:q /filelogger BuildAll.csproj    
 }
 
-Task Tests -Depends Build {
+Task Tests {
     & install\build\bin\oscript.exe tests\testrunner.os -runall tests
 }
 
